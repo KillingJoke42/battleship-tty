cd38a0614df8009866f0dfe424dae4c50731358c
KillingJoke42 <anantraina1999@gmail.com>
2022-07-28 18:32:12 +0530

    Adds string to morse conversion

--- src/main.c
+++ src/main.c
@@ -4,25 +4,13 @@
 
 int main(void)
 {
-    // char* str = read_dynamic_string();
-    // if (str == NULL)
-    //     return -1;
-    char* test;
-    // for (int i = '0'; i <= '9'; i++)
-    // {
-    // test = char_to_morseletter(i);
-    // printf("Entered string is: %s\n", test);
-    // }
-    for (int i = '0'; i <= '9'; i++)
-    {
-    test = char_to_morseletter(i);
-    printf("Entered string %c is: %s\n", i, test);
-    }
-    for (int i = 'A'; i <= 'Z'; i++)
-    {
-    test = char_to_morseletter(i);
-    printf("Entered string %c is: %s\n", i, test);
-    }
-    free(test);
+    char* str = read_dynamic_string();
+    if (str == NULL)
+        return -1;
+
+    char* morsestr = string_to_morse(str);
+    printf("Entered string is: %s\n", morsestr);
+    free(str);
+    free(morsestr);
     return 0;
 }
