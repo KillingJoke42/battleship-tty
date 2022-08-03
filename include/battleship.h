9b351432249d204a6d5133212f1100221d9848a4
KillingJoke42 <anantraina1999@gmail.com>
2022-08-04 02:31:26 +0530

    placement phase for TTY complete

--- include/battleship.h
+++ include/battleship.h
@@ -30,19 +30,12 @@ enum ships {
 #define SHIP_SUBMARINE_SZ   3
 #define SHIP_PATROL_BOAT_SZ  2
 
-char ship_name_map[][11] = {"carrier", "battleship", "destroyer", "submarine", "patrol boat"};
-char ship_sz_map[] = {SHIP_CARRIER_SZ, SHIP_BATTLESHIP_SZ, SHIP_DESTROYER_SZ, SHIP_SUBMARINE_SZ, SHIP_PATROL_BOAT_SZ};
+static char ship_name_map[][11] = {"carrier", "battleship", "destroyer", "submarine", "patrol boat"};
+static char ship_sz_map[] = {SHIP_CARRIER_SZ, SHIP_BATTLESHIP_SZ, SHIP_DESTROYER_SZ, SHIP_SUBMARINE_SZ, SHIP_PATROL_BOAT_SZ};
 
 #define NUM_ROWS    10
 #define NUM_COLS    10
 #define NUM_SHIPS   5
-
-typedef struct {
-    char *playerName;
-    uint8_t idx;
-    uint8_t playerPlacement[NUM_ROWS][NUM_COLS];
-    ship_status_t player_ship_status;
-} player_t;
 
 typedef struct {
     uint8_t carrier[SHIP_CARRIER_SZ];
@@ -52,9 +45,17 @@ typedef struct {
     uint8_t patrol_boat[SHIP_PATROL_BOAT_SZ];
 } ship_status_t;
 
-void phase_place_ships(char player_arr[][]);
+typedef struct {
+    char *playerName;
+    uint8_t idx;
+    uint8_t playerPlacement[NUM_ROWS][NUM_COLS];
+    ship_status_t player_ship_status;
+} player_t;
+
+void phase_place_ships(player_t *player);
 uint8_t* phase_fire(void);
-uint8_t find_arr_sum(char* arr, int range);
-uint8_t find_vertical_sum(char arr[][], char ship_row, char ship_col, int range);
+uint8_t find_arr_sum(uint8_t* arr, int range);
+uint8_t find_vertical_sum(uint8_t arr[NUM_ROWS][NUM_COLS], char ship_row, char ship_col, int range);
 uint8_t is_ship_destroyed(ship_status_t *player_ship_status, char ship_type);
+void print_player_placements(uint8_t player_arr[NUM_ROWS][NUM_COLS]);
 #endif
--- src/main.c
+++ src/main.c
@@ -1,16 +1,15 @@
 #include <morse_interpreter.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <tcptools.h>
+#include <battleship.h>
 
 int main(void)
 {
-    char* str = read_dynamic_string();
-    if (str == NULL)
-        return -1;
+    player_t *player = (player_t *)malloc(sizeof(player_t));
+    memset(player->playerPlacement, 0, sizeof(player->playerPlacement));
+    phase_place_ships(player);
 
-    char* morsestr = string_to_morse(str);
-    printf("Entered string is: %s\n", morsestr);
-    free(str);
-    free(morsestr);
     return 0;
 }
