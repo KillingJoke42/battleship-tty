59ce11670f11dccbac5de3f219fbe8674734d87b
KillingJoke42 <anantraina1999@gmail.com>
2022-08-09 16:29:38 +0530

    Adds random player idx assign and minor formatting fixes

--- include/utils.h
+++ include/utils.h
@@ -8,8 +8,15 @@
 
 #define clrscr() printf("\e[H\e[2J\e[3J")
 
+typedef struct rng_mem{
+    uint8_t val;
+    struct rng_mem* next;
+} rng_mem_t;
+
 char* get_string(char* prompt);
 uint8_t** alloc_dynamic_2d_arr(char num_rows, char num_cols);
 void clear_2d_arr(uint8_t **arr, uint8_t val, uint8_t num_rows, uint8_t num_cols);
 void enter_wait_prompt(char* prompt);
+uint8_t* alloc_rng_arr(uint8_t sz);
+uint8_t get_rng_val(uint8_t* arr, uint8_t* sz);
 #endif
